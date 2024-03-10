5b7137c69cbb518aa22657377f0a63534466b5ff
Jorge <Jorge@LAPTOP-13O2AL1N>
2024-03-10 20:29:12 -0300

    Terceiro Programa em Java Figura geometrica

---
+++ TerceiroProjetoJava/src/main/java/com/mycompany/terceiroprojetojava/FiguraGeometrica.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.terceiroprojetojava;
+
+/**
+ *
+ * @author Jorge
+ */
+import javax.swing.*;
+
+public class FiguraGeometrica {	
+
+	private String cor;		
+
+	public FiguraGeometrica() {
+		this("");	
+	}	
+	public FiguraGeometrica(String cor){			
+		setCor(cor);	
+	}	
+	public String getCor() {		
+		return cor;	
+	}	
+	public void setCor( String cor ) {		
+		this.cor = cor;	
+	}	
+	public void leitura() {	
+		setCor(JOptionPane.showInputDialog("Digite a Cor"));	
+	}	
+	public void imprimir() {		
+		JOptionPane.showMessageDialog(null,"A cor é " + getCor());	
+	}	
+	public String paraString() {		
+		return ("Cor : " + getCor());	
+	}
+}
+
